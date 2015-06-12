34296c0ab66619a541076318994f7ec454df2532
Manuel Zavatta <manuel.zavatta@gmail.com>
2015-06-12 10:23:29 +0200

    Widget update

--- widget.inc.php
+++ widget.inc.php
@@ -7,18 +7,11 @@ require_once("../core/api.inc.php");
 api_loadLocaleFile("../workflows/");
 // include module api
 require_once("../workflows/api.inc.php");
-/*// include registries api
-require_once("../registries/api.inc.php");
-// include materials api
-require_once("../materials/api.inc.php");*/
 // widget title and well
 echo "<h4>".api_text("widget-title")."</h4>\n";
 echo "<div class='well well-small well-white'>\n";
-
 // acquire variables
 $span=$_GET['span'];
-
-
 // if span < 6 build small widget
 if($span<6){
  // check personal workflows
@@ -54,109 +47,164 @@ if($span<6){
  echo "<h5>".api_text("widget-tickets")."</h5>\n".$processable_tickets;
 }else{
  // if span > 6 build large widget
-
-// definitions
-$details_modals_array=array();
-// acquire workflows status filter
-$workflows_query="(status='1' OR status='2' OR status='3')";
-// check for personal workflows
-if($GLOBALS['db']->countOf("workflows_workflows","addIdAccount='".$_SESSION['account']->id."' AND ".$workflows_query)>0){
- // build workflows table
- $workflows_table=new str_table(api_text("flows-tr-workflowsUnvalued"),TRUE);
- $workflows_table->addHeader("&nbsp;",NULL,"16");
- $workflows_table->addHeader(api_text("flows-th-timestamp"),"nowarp");
- $workflows_table->addHeader("&nbsp;",NULL,"16");
- if($span>8){$workflows_table->addHeader("!","nowarp text-center");}
- if($span>8){$workflows_table->addHeader(api_text("flows-th-category"),"nowarp");}
- $workflows_table->addHeader(api_text("flows-th-subject"),NULL,"100%");
+ $query_where="status<='3'";
+ // only assignable tickets
+ $query_where.=" AND ( idAssigned='".$_SESSION['account']->id."'";
+ foreach(api_account()->companies[api_company()->id]->groups as $group){$query_where.=" OR idGroup='".$group->id."'";}
+ $query_where.=" )";
+ // order tickets
+ $query_order=api_queryOrder("addDate DESC");
+ // acquire tickets
+ $tickets=$GLOBALS['db']->query("SELECT * FROM workflows_tickets WHERE ".$query_where.$query_order);
+ while($ticket=$GLOBALS['db']->fetchNextObject($tickets)){$tickets_array[]=api_workflows_ticket($ticket);}
+ // query where
+ if($g_workflows<>"all"){$query_where=" AND (status<='3' OR addDate>=(NOW()-INTERVAL 15 DAY))";}else{$query_where=NULL;}
  // build workflow table rows
- $workflows=$GLOBALS['db']->query("SELECT * FROM workflows_workflows WHERE addIdAccount='".$_SESSION['account']->id."' AND ".$workflows_query." ORDER BY addDate DESC");
- while($workflow=$GLOBALS['db']->fetchNextObject($workflows)){
-  $workflows_table->addRow();
-  // build workflows table fields
-  $workflows_table->addField("<a href='../workflows/workflows_view.php?id=".$workflow->id."'>".api_icon("icon-search")."</a>","nowarp");
-  $workflows_table->addField(api_timestampFormat($workflow->addDate,api_text("date")),"nowarp");
-  $workflows_table->addField(api_workflows_status($workflow->status,TRUE),"nowarp text-center");
-  if($span>8){$workflows_table->addField($workflow->priority,"nowarp text-center");}
-  if($span>8){$workflows_table->addField(api_workflows_categoryName($workflow->idCategory,TRUE,TRUE,TRUE),"nowarp");}
-  $workflows_table->addField(stripslashes($workflow->subject));
- }
-}
-// generate tickets query
-$query_where="(status='1' OR status='2' OR status='3')";
-// only assignable tickets
-$query_where.=" AND ( ";
-$query_where.=" idAssigned='".$_SESSION['account']->id."'";
-foreach(api_account()->companies[api_company()->id]->groups as $group){$query_where.=" OR idGroup='".$group->id."'";}
-if(api_accountGroupMember(1)){$query_where.=" OR idGroup='0'";}
-$query_where.=" )";
-// build tickets table
-$tickets_table=new str_table(api_text("flows-tr-ticketsUnvalued"),TRUE);
-$tickets_table->addHeader("&nbsp;",NULL,"16");
-$tickets_table->addHeader(api_text("flows-th-timestamp"),"nowarp");
-$tickets_table->addHeader("&nbsp;",NULL,"16");
-if($span>8){$tickets_table->addHeader(api_text("flows-th-sla"),"nowarp text-center");}
-if($span>8){$tickets_table->addHeader("!","nowarp text-center");}
-$tickets_table->addHeader(api_text("flows-th-account"),"nowarp");
-if($span>8){$tickets_table->addHeader(api_text("flows-th-category"),"nowarp");}
-$tickets_table->addHeader(api_text("flows-th-subject"),NULL,"100%");
-if($span>8){$tickets_table->addHeader(api_text("flows-th-assigned"),"nowarp");}
-$tickets_table->addHeader(api_text("flows-th-group"),"nowarp text-center");
-$tickets_table->addHeader("&nbsp;",NULL,"16");
-// build tickets table rows
-$tickets=$GLOBALS['db']->query("SELECT * FROM workflows_tickets WHERE ".$query_where." ORDER BY addDate DESC");
-while($ticket=$GLOBALS['db']->fetchNextObject($tickets)){
- $tickets_table->addRow();
- // assigned id
- if(!$ticket->idAssigned){$ticket->idAssigned=0;}
- // details modal windows
- $details_modal=api_workflows_ticketDetailsModal($ticket);
- $details_modals_array[]=$details_modal;
- // build tickets table fields
- $tickets_table->addField("<a href='../workflows/workflows_view.php?id=".$ticket->idWorkflow."&idTicket=".$ticket->id."'>".api_icon("icon-search")."</a>","nowarp");
- if($span>8){$tickets_table->addField(api_timestampFormat($ticket->addDate,api_text("datetime")),"nowarp");}
-  else{$tickets_table->addField(api_timestampFormat($ticket->addDate,api_text("datetime-noyear")),"nowarp");}
- $tickets_table->addField(api_workflows_status($ticket->status,TRUE,$ticket->solved),"nowarp text-center");
- if($span>8){$tickets_table->addField(api_workflows_ticketSLA($ticket),"nowarp text-center");}
- if($span>8){$tickets_table->addField($ticket->priority,"nowarp text-center");}
- $tickets_table->addField(api_account($ticket->addIdAccount)->firstname,"nowarp");
- if($span>8){$tickets_table->addField(api_workflows_categoryName($ticket->idCategory,TRUE,TRUE,TRUE),"nowarp");}
- $tickets_table->addField(stripslashes($ticket->subject));
- if($span>8){$tickets_table->addField(api_account($ticket->idAssigned)->firstname,"nowarp text-right");}
- $tickets_table->addField(api_groupName($ticket->idGroup,TRUE,TRUE),"nowarp text-center");
- $tickets_table->addField($details_modal->link(api_icon("icon-list")),"nowarp text-center");
-}
-
-
+ $workflows=$GLOBALS['db']->query("SELECT * FROM workflows_workflows WHERE addIdAccount='".api_account()->id."'".$query_where." ORDER BY addDate DESC");
+ // if span < 8 build large widget
  if($span<8){
-
-
-
+  // build tickets table
+  $tickets_table=new str_table(api_text("workflows-tr-ticketsUnvalued"),TRUE);
+  $tickets_table->addHeader("&nbsp;",NULL,"16");
+  $tickets_table->addHeader("!","nowarp text-center","16");
+  $tickets_table->addHeader(api_text("workflows-th-account"),"nowarp");
+  $tickets_table->addHeader(api_text("workflows-th-category"),"nowarp");
+  $tickets_table->addHeader(api_text("workflows-th-subject"),NULL,"100%");
+  // build tables
+  $tables_array['opened']=$tickets_table;
+  $tables_array['assigned']=clone $tickets_table;
+  $tables_array['standby']=clone $tickets_table;
+  // group in opened table
+  $tickets_table->addHeader(api_text("workflows-th-group"),"nowarp text-center");
+  // build tickets table rows
+  foreach($tickets_array as $ticket){
+   // definitions
+   $table=NULL;
+   if(!$ticket->idAssigned){$ticket->idAssigned=0;}
+   // status
+   if($ticket->status==1){$table="opened";}
+   if($ticket->status==2&&$ticket->idAssigned==api_account()->id){$table="assigned";}
+   if($ticket->status==3&&$ticket->idAssigned==api_account()->id){$table="standby";}
+   // check table
+   if(!$table){continue;}
+   // details modal windows
+   $details_modal=api_workflows_ticketDetailsModal($ticket);
+   $tickets_modals_array[]=$details_modal;
+   // check urged
+   if($ticket->urged){$tr_class="error";}else{$tr_class=NULL;}
+   // buil row
+   $tables_array[$table]->addRow($tr_class);
+   // build tickets table fields
+   $tables_array[$table]->addField("<a href='../workflows/workflows_view.php?id=".$ticket->idWorkflow."&idTicket=".$ticket->id."' target='_blank'>".api_icon("icon-search")."</a>","nowarp");
+   $tables_array[$table]->addField(api_workflows_ticketPriority($ticket->priority,TRUE),"nowarp text-center");
+   $tables_array[$table]->addField(api_workflows_referentName($ticket->idWorkflow),"nowarp");
+   $tables_array[$table]->addField(api_workflows_categoryName($ticket->idCategory,TRUE,TRUE,TRUE),"nowarp");
+   $tables_array[$table]->addField(stripslashes($ticket->subject));
+   if($table=="opened"){$tables_array[$table]->addField(api_groupName($ticket->idGroup,TRUE,TRUE),"nowarp text-center");}
+  }
+  // build personal workflows table
+  $workflows_table=new str_table(api_text("workflows-tr-workflowsUnvalued"),FALSE);
+  $workflows_table->addHeader("&nbsp;",NULL,"16");
+  $workflows_table->addHeader("#","nowarp text-center");
+  $workflows_table->addHeader(api_text("workflows-th-timestamp"),"nowarp");
+  $workflows_table->addHeader(api_text("workflows-th-subject"),NULL,"100%");
+  // cycle personal workflows
+  while($workflow=$GLOBALS['db']->fetchNextObject($workflows)){
+   $workflows_table->addRow();
+   // build workflows table fields
+   $workflows_table->addField("<a href='../workflows/workflows_view.php?id=".$workflow->id."' target='_blank'>".api_icon("icon-search")."</a>","nowarp");
+   $workflows_table->addField(str_pad($workflow->id,5,"0",STR_PAD_LEFT),"nowarp");
+   $workflows_table->addField(api_timestampFormat($workflow->addDate,api_text("datetime")),"nowarp");
+   $workflows_table->addField(stripslashes($workflow->subject));
+  }
  }else{
-
-
-
+  // if span > 8 build large widget
+  // build tickets table
+  $tickets_table=new str_table(api_text("workflows-tr-ticketsUnvalued"),TRUE);
+  $tickets_table->addHeader("&nbsp;",NULL,"16");
+  $tickets_table->addHeader("#","nowarp text-center");
+  $tickets_table->addHeader("!","nowarp text-center","16");
+  $tickets_table->addHeader(api_text("workflows-th-timestamp"),"nowarp");
+  $tickets_table->addHeader(api_text("workflows-th-account"),"nowarp");
+  $tickets_table->addHeader(api_text("workflows-th-category"),"nowarp");
+  $tickets_table->addHeader(api_text("workflows-th-subject"),NULL,"100%");
+  // build tables
+  $tables_array['opened']=$tickets_table;
+  $tables_array['assigned']=clone $tickets_table;
+  $tables_array['standby']=clone $tickets_table;
+  // group in opened table
+  $tickets_table->addHeader(api_text("workflows-th-group"),"nowarp text-center");
+  // build tickets table rows
+  foreach($tickets_array as $ticket){
+   // definitions
+   $table=NULL;
+   if(!$ticket->idAssigned){$ticket->idAssigned=0;}
+   // status
+   if($ticket->status==1){$table="opened";}
+   if($ticket->status==2&&$ticket->idAssigned==api_account()->id){$table="assigned";}
+   if($ticket->status==3&&$ticket->idAssigned==api_account()->id){$table="standby";}
+   // check table
+   if(!$table){continue;}
+   // details modal windows
+   $details_modal=api_workflows_ticketDetailsModal($ticket);
+   $tickets_modals_array[]=$details_modal;
+   // check urged
+   if($ticket->urged){$tr_class="error";}else{$tr_class=NULL;}
+   // buil row
+   $tables_array[$table]->addRow($tr_class);
+   // build tickets table fields
+   $tables_array[$table]->addField("<a href='../workflows/workflows_view.php?id=".$ticket->idWorkflow."&idTicket=".$ticket->id."' target='_blank'>".api_icon("icon-search")."</a>","nowarp");
+   $tables_array[$table]->addField($ticket->number,"nowarp");
+   $tables_array[$table]->addField(api_workflows_ticketPriority($ticket->priority,TRUE),"nowarp text-center");
+   $tables_array[$table]->addField(api_timestampFormat($ticket->addDate,api_text("datetime")),"nowarp");
+   $tables_array[$table]->addField(api_workflows_referentName($ticket->idWorkflow),"nowarp");
+   $tables_array[$table]->addField(api_workflows_categoryName($ticket->idCategory,TRUE,TRUE,TRUE),"nowarp");
+   $tables_array[$table]->addField(stripslashes($ticket->subject));
+   if($table=="opened"){$tables_array[$table]->addField(api_groupName($ticket->idGroup,TRUE,TRUE),"nowarp text-center");}
+  }
+  // build personal workflows table
+  $workflows_table=new str_table(api_text("workflows-tr-workflowsUnvalued"),FALSE);
+  $workflows_table->addHeader("&nbsp;",NULL,"16");
+  $workflows_table->addHeader("#","nowarp text-center");
+  $workflows_table->addHeader("!","nowarp text-center");
+  $workflows_table->addHeader(api_text("workflows-th-timestamp"),"nowarp");
+  $workflows_table->addHeader(api_text("workflows-th-category"),"nowarp");
+  $workflows_table->addHeader(api_text("workflows-th-subject"),NULL,"100%");
+  $workflows_table->addHeader(api_text("workflows-th-status"),"nowarp text-right","16");
+  // cycle personal workflows
+  while($workflow=$GLOBALS['db']->fetchNextObject($workflows)){
+   $workflows_table->addRow();
+   // build workflows table fields
+   $workflows_table->addField("<a href='../workflows/workflows_view.php?id=".$workflow->id."' target='_blank'>".api_icon("icon-search")."</a>","nowarp");
+   $workflows_table->addField(str_pad($workflow->id,5,"0",STR_PAD_LEFT),"nowarp");
+   $workflows_table->addField(api_workflows_ticketPriority($workflow->priority,TRUE),"nowarp text-center");
+   $workflows_table->addField(api_timestampFormat($workflow->addDate,api_text("datetime")),"nowarp");
+   $workflows_table->addField(api_workflows_categoryName($workflow->idCategory,TRUE,TRUE,TRUE),"nowarp");
+   $workflows_table->addField(stripslashes($workflow->subject));
+   $workflows_table->addField(api_workflows_status($workflow->status),"nowarp text-right'");
+  }
  }
-
-
-// show workflows table
-if(is_object($workflows_table)){
- echo "<h5>".api_text("flows-workflows")."</h5>\n";
- $workflows_table->render();
+ // show opened tickets table
+ if($tables_array["opened"]->count()){
+  echo "<h5>".api_text("workflows-tickets-opened")."</h5>\n";
+  $tables_array["opened"]->render();
+ }
+ // show assigned tickets table
+ if($tables_array["assigned"]->count()){
+  echo "<h5>".api_text("workflows-tickets-assigned")."</h5>\n";
+  $tables_array["assigned"]->render();
+ }
+ // show standby tickets table
+ if($tables_array["standby"]->count()){
+  echo "<h5>".api_text("workflows-tickets-standby")."</h5>\n";
+  $tables_array["standby"]->render();
+ }
+ // show workflows table
+ if(is_object($workflows_table)){
+  echo "<h5>".api_text("workflows-workflows")."</h5>\n";
+  $workflows_table->render();
+ }
 }
-if(is_object($workflows_table) && is_object($tickets_table)){echo "<hr>\n";}
-// show tickets table
-if(is_object($tickets_table)){
- echo "<h5>".api_text("flows-tickets")."</h5>\n";
- $tickets_table->render();
-}
-// show the pagination
-if(is_object($pagination)){$pagination->render();}
-// show status modal windows
-foreach($details_modals_array as $modal){$modal->render();}
-
-}
-
 echo "<span class='pull-right'>\n";
 echo "<a href='../workflows/index.php'>".api_text("widget-showAll")."</a>\n";
 echo "</span>\n";
