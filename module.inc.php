7c660aad48efb1745dff933a310d797e39e29488
Manuel Zavatta <manuel.zavatta@gmail.com>
2016-09-21 15:57:04 +0200

    Release 1.3.29

--- module.inc.php
+++ module.inc.php
@@ -3,7 +3,7 @@
 |* -[ Workflows - Module Information ]--------------------------------------- *|
 \* -------------------------------------------------------------------------- */
 $module_name="workflows";
-$module_version="1.3.28";
+$module_version="1.3.29";
 $module_title="Workflows";
 $module_description="Workflows Ticketing System";
 $module_create_menu=TRUE;
